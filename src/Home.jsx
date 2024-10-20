75478d01b86d28e8d8aebaabef97a6ce86b925f9
alphamuhia <muhiaalpha@gmail.com>
2024-10-20 16:36:17 +0300

    done

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="box-border border-black bg-gray-400 absolute-center">
+        <h1>
+          Welcome to Alpha's faverites, your ultimate destination for all things
+          books and manga! Whether you're a passionate reader, or a manga
+          enthusiast, we've got something for everyone. Explore our diverse
+          collection of stories and immerse yourself in stunning visuals. Stay
+          updated on the latest releases, discover hidden gems, and connect with
+          a vibrant community of like-minded fans. Dive into a world of
+          creativity and entertainment that caters to your every interest!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
